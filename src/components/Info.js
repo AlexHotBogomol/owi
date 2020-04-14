d6947fd37f11365eeeefff53f95bd4a005c1e496
AlexHotBogomol <Zakablukov777@gmail.com>
2020-04-14 15:45:03 +0300

    add 8 questions

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Title = () => (
+  <div className="title">
+    <h1>Dateneingabe</h1>
+    <p>
+      Zur zeitnahen Bearbeitung und Verfahrensbeschleunigung möchten wir Sie bitten,
+      die nachstehenden Fragen vollständig und richtig zu beantworten. Hierfür vielen Dank!
+    </p>
+    <p>* Pflichtfeld</p>
+  </div>
+);
+
+export default Title
